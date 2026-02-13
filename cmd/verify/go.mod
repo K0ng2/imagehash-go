d04796795fcd8f5d7f07468b782c17cf2cb0635f
K0ng2 <dev@k0ng.page>
2026-02-13 16:31:50 +0700

    feat(verify): add new image hashing algorithms

---
+++ cmd/verify/go.mod
@@ -0,0 +1,14 @@
+module verify
+
+go 1.25.0
+
+require (
+	github.com/K0ng2/imagehash-go v1.1.0
+	github.com/corona10/goimagehash v1.1.0
+)
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	golang.org/x/image v0.36.0 // indirect
+)
--- cmd/verify/main.go
+++ cmd/verify/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	imagehashgo "github.com/K0ng2/imagehash-go"
+	"github.com/corona10/goimagehash"
 )
 
 func main() {
@@ -26,8 +27,25 @@ func main() {
 	dhash := imagehashgo.DifferenceHash(img, 8)
 	dhashV := imagehashgo.DifferenceHashVertical(img, 8)
 
+	ahash1, err := goimagehash.AverageHash(img)
+	if err != nil {
+		panic(err)
+	}
+	phash1, err := goimagehash.ExtPerceptionHash(img, 8, 8)
+	if err != nil {
+		panic(err)
+	}
+	dhash1, err := goimagehash.DifferenceHash(img)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("ahash: %s\n", ahash.ToString())
 	fmt.Printf("phash: %s\n", phash.ToString())
 	fmt.Printf("dhash: %s\n", dhash.ToString())
 	fmt.Printf("dhash_v: %s\n", dhashV.ToString())
+
+	fmt.Printf("ahash1: %s\n", ahash1.ToString())
+	fmt.Printf("phash1: %s\n", phash1.ToString())
+	fmt.Printf("dhash1: %s\n", dhash1.ToString())
 }
